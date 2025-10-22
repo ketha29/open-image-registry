36c1117223d9cc9b24c94fb7c26eadef24034175
ksankeerth <shankeerthan1995@gmail.com>
2025-10-22 08:09:33 +0530

    [UI] Added new types

--- webapp/src/types/app_types.ts
+++ webapp/src/types/app_types.ts
@@ -5,7 +5,7 @@ export enum RegistryTypes {
 
 export enum ImageTagsTreeNodeTypes {
   Registry,
-  Namespace, 
+  Namespace,
   Repository,
   Tag,
 }
@@ -20,3 +20,73 @@ export type ImageTagsTreeViewNodeData = {
   tag: string;
 };
 
+export type NamespaceAccess = {
+  id: string;
+  namespace: string;
+  resource_id: string;
+  user_id: string;
+  access_level: string;
+  granted_by: string;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export type RepositoryAccesss = {
+  id: string;
+  namespace: string;
+  repository: string;
+  resource_id: string;
+  user_id: string;
+  access_level: string;
+  granted_by: string;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export type UserProfileInfo = {
+  user_id: string;
+  username: string;
+  role: string;
+  namespaces: NamespaceAccess[];
+  repositories: RepositoryAccesss[];
+};
+
+export type UserAccountInfo = {
+  id: string;
+  username: string;
+  role: string;
+  email: string;
+  display_name: string;
+  locked: boolean;
+  locked_reason: string;
+  failed_attempts: number;
+  locked_at?: Date;
+  password_recovery_id: string;
+  password_recovery_reason: string;
+  password_recovery_at?: Date;
+  last_loggedin_at?: Date;
+  created_at: Date;
+  updated_at?: Date;
+};
+
+export type TableSortState<T> = {
+  key: keyof T;
+  order: -1 | 0 | 1;
+};
+
+export type TablePaginationState = {
+  page: number;
+  limit: number;
+};
+
+export type TableColumnFilterState<T, K extends keyof T = keyof T> = {
+  key: keyof T;
+  values:  T[K][]; // equal filter only for simplicity
+}
+
+export type TableFilterSearchPaginationSortState<T> = {
+  sort?: TableSortState<T>;
+  pagination: TablePaginationState;
+  filters?: TableColumnFilterState<T>[];
+  search_value?: string;
+};
